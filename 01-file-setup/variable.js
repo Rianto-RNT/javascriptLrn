74eb445e3f61dafd49ee2e4aa2faceed58c2d614
Rianto-RNT <rianto.rnt@gmail.com>
2021-08-22 16:34:52 +0800

    fix variable

---
+++ 01-file-setup/variable.js
@@ -0,0 +1,11 @@
+var firstName = "Al"
+var lastName = "Fatih"    
+    console.log(firstName, lastName)
+
+name = "rian"
+console.log(name)
+
+var message
+console.log(message)
+message = "New message!"
+console.log(message)
