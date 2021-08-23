1156412f632a0e1fdce29e5af06bedecc2c52b12
Rianto-RNT <rianto.rnt@gmail.com>
2021-08-23 08:02:30 +0800

    + let and const - part 1

---
+++ 01-file-setup/let_const.js
@@ -0,0 +1,18 @@
+// let name 
+
+// name = "Al"
+// console.log(name)
+// name ="Rian"
+// console.log(name)
+
+// const name = "Khairi"
+// name = "Hafidz"
+
+const user = {
+    name: 'Al',
+    age: 17
+}
+console.log(user)
+user.name= "Khairi"
+console.log(user)
+
