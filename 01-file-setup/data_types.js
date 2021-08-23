355e96171727afb56bd72ee85f4eabfe4fbbd6f8
Rianto-RNT <rianto.rnt@gmail.com>
2021-08-23 08:22:16 +0800

    + data type - part 1

---
+++ 01-file-setup/data_types.js
@@ -0,0 +1,25 @@
+// premitive
+// undefined
+let item
+// String
+item = 'Hello'
+// Number
+item = 1
+// Boolean
+item = true
+// null
+item = null
+// Symbol
+item = Symbol()
+
+// reference type
+
+// Array
+item = [1, 2, 4]
+// Object
+item = {
+    name: 'Rian'
+}
+// Date
+item = new Date()
+console.log('type', typeof item)
