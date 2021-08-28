685d3c2d12b303e92c22ebc317b4c15e0252ff58
Rianto-RNT <rianto.rnt@gmail.com>
2021-08-28 09:43:28 +0800

    + use variable name with let

--- 01-file-setup/app.js
+++ 01-file-setup/app.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 //test
 console.log("Hi, i'm rian")
-=======
+
 console.log("Hi, i'm rianto")
->>>>>>> 4074e615ce2a5459772c3314f967d8f62ec7fd33
+
 
--- basic/variable.js
+++ basic/variable.js
@@ -1,3 +1,11 @@
-function myFunction(){
-    
-}
+let name  = 'Rian';
+console.log(name);
+
+// cannot be a reserved keyword
+// Should be meaningful
+// cannot start with a number example(1name)
+// cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+let firtName = 'ryan';
+let lastName = 'Morrison';
