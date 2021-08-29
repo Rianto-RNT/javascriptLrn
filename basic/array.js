ebc5cea94aadb18aea7628af50b7210d826f5381
Rianto-RNT <rianto.rnt@gmail.com>
2021-08-29 19:40:34 +0800

    + more basic

---
+++ basic/array.js
@@ -0,0 +1,11 @@
+//list product at shopping cart
+
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 'green';
+selectedColors[3] = '14';
+
+// console.log(selectedColors[0])
+
+// console.log(selectedColors)
+
+console.log(selectedColors.length);
---
+++ basic/typeOfFunctions.js
@@ -0,0 +1,22 @@
+// perform a task
+function greet(firstName, lastName) {
+    console.log('Hello ' + firstName + ' ' + lastName);
+}
+
+//Calculating a value
+function square(number) {
+    return number * number;
+}
+
+// let number = greet(2); 
+// console.log(number);
+
+//or
+
+console.log(square(2));
+
+//ors
+
+console.log('Hello');
+
+
