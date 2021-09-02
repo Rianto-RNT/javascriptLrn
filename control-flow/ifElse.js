54d81c1568276c0481f19d0bd2a939937b30804e
Rianto-RNT <rianto.rnt@gmail.com>
2021-09-02 16:37:34 +0800

    + if else

---
+++ control-flow/ifElse.js
@@ -0,0 +1,21 @@
+// if (condition) {
+//     statement
+// }
+// else if (anotherCondition) {
+//     statement
+// }
+// else if (yetAnotherCondition)
+//     statement
+//  else 
+//   statement 
+
+let hour = 23;
+
+if (hour >= 6 && hour < 12) {
+    console.log('Good Morning');
+}
+else if (hour >= 12 && hour < 18) {
+    console.log('Good Afternoon')
+}
+ else 
+  console.log('Good Evening')
